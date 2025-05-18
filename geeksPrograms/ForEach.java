2caae11f77edf48df301b2502c4630fb388fb956
Rishabh31714 <rkaushik255@gmail.com>
2025-05-18 18:31:10 +0530

    made new practice code

---
+++ geeksPrograms/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50, 60 };
+        for (int num : arr) {
+            System.out.println("Value :" + num);
+        }
+    }
+}
