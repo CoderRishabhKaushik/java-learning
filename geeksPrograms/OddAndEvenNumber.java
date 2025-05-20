eb69d39232ae29c5d847d3b6ac624a6c8ee06e8c
Rishabh31714 <rkaushik255@gmail.com>
2025-05-20 21:37:20 +0530

    made new program using only if else system

---
+++ geeksPrograms/OddAndEvenNumber.java
@@ -0,0 +1,11 @@
+public class OddAndEvenNumber {
+    public static void main(String[] args) {
+        int num = 11;
+
+        if (num % 2 == 0) {
+            System.out.println("This is the even number");
+        } else {
+            System.out.println("This is the odd number");
+        }
+    }
+}
